2caa85af24b5d208f691938fbef695aee158c59a
Daniel Wikander <daniel.wikander@gmail.com>
2019-02-15 13:45:01 +0100

    Changed structure and removed duplicates

--- scripts/screendetector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-intern=eDP1
-extern=HDMI-1-2
-
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
-else
-    xrandr --output "$intern" --primary --auto --output "$extern" --right-of "$intern" --auto
-fi
